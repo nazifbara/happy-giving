6c961c4a0ebc60758754587d771035ad9ea82f91
nazifbara <nazifbarassounon@gmail.com>
2022-03-02 07:25:37 +0100

    add tab navigation

--- src/routes/ProjectView.js
+++ src/routes/ProjectView.js
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { CircularProgress, Container, Stack, Typography } from '@mui/material';
+import {
+  CircularProgress,
+  Container,
+  Stack,
+  Typography,
+  Tabs,
+  Tab,
+  Box,
+  Paper,
+} from '@mui/material';
 
 import { useProject } from '../hooks';
 
 function ProjectView() {
   const { projectId } = useParams();
+  const [tabValue, setTabValue] = useState(0);
+
+  const handleTabChange = (e, newValue) => setTabValue(newValue);
+
   const { data: project, isSuccess, isLoading } = useProject(projectId);
   console.log({ project });
 
@@ -22,11 +36,33 @@ function ProjectView() {
           >
             {project.title}
           </Typography>
+
           <img src={project.image.imagelink[4].url} alt="" />
+
+          <Tabs value={tabValue} onChange={handleTabChange} centered>
+            <Tab label="Story" />
+            <Tab label="Photos" />
+          </Tabs>
+
+          <Paper sx={{ px: 3, py: 4 }}>
+            <TabPanel value={tabValue} index={0}>
+              Project Story
+            </TabPanel>
+            <TabPanel value={tabValue} index={1}>
+              Project Photos
+            </TabPanel>
+          </Paper>
         </Stack>
       )}
     </Container>
   );
+}
+
+function TabPanel({ value, index, children }) {
+  if (value !== index) {
+    return null;
+  }
+  return <Box>{children}</Box>;
 }
 
 const route = {
